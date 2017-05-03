4eada2e76498e58468d212aca2a76e5622e0527d
kkimigawa <kkimigawa@gmail.com>
2017-05-03 19:50:46 +0900

    [change] Magit open current window

--- inits/20_magit.el
+++ inits/20_magit.el
@@ -2,3 +2,21 @@
 
 ;; diff内の変更点を色分けする
 (setq magit-diff-refine-hunk 'all)
+
+;; 同じウインドウでmagitを開く
+(setq magit-display-buffer-function
+      (lambda (buffer)
+        (display-buffer
+         buffer
+         (cond ((and (derived-mode-p 'magit-mode)
+                     (eq (with-current-buffer buffer major-mode)
+                         'magit-status-mode))
+                nil)
+               ((memq (with-current-buffer buffer major-mode)
+                      '(magit-process-mode
+                        magit-revision-mode
+                        magit-diff-mode
+                        magit-stash-mode))
+                nil)
+               (t
+                '(display-buffer-same-window))))))
