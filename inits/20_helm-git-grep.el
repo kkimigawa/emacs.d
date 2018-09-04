4cb90b3d702afbde4574e7606a3b0569976e9512
kkimigawa <kkimigawa@gmail.com>
2018-09-04 22:38:38 +0900

    Revert "[change] Change helm-git-grep to helm-grep-do-git-grep"
    This reverts commit 8cb77d40e06793cbf8740d9ad83325c1b65c98c4.

--- inits/20_helm-git-grep.el
+++ inits/20_helm-git-grep.el
@@ -1,6 +1,1 @@
-(defun helm-grep-do-git-grep-all ()
-  (interactive)
-  (helm-grep-do-git-grep t))
-
-;;(global-set-key (kbd "C-c g g") 'helm-git-grep)
-(global-set-key (kbd "C-c g g") 'helm-grep-do-git-grep-all)
+(global-set-key (kbd "C-c g g") 'helm-git-grep)
