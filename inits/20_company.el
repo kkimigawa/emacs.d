e25c9100c8af5669557dae523469f6c5e3b0d7c5
kkimigawa <kkimigawa@gmail.com>
2018-12-04 23:43:50 +0900

    [change] Change company-mode color (like a auto-complete-mode)

--- inits/20_company.el
+++ inits/20_company.el
@@ -19,3 +19,19 @@
       (indent-for-tab-command)
     (company-complete)))
 (global-set-key (kbd "TAB") 'my-company-key)
+
+;; カラーをauto-completeのようにする
+(set-face-attribute 'company-tooltip nil
+                    :foreground "black" :background "lightgrey")
+(set-face-attribute 'company-tooltip-common nil
+                    :foreground "black" :background "lightgrey")
+(set-face-attribute 'company-tooltip-common-selection nil
+                    :foreground "white" :background "steelblue")
+(set-face-attribute 'company-tooltip-selection nil
+                    :foreground "black" :background "steelblue")
+(set-face-attribute 'company-preview-common nil
+                    :background nil :foreground "lightgrey" :underline t)
+(set-face-attribute 'company-scrollbar-fg nil
+                    :background "orange")
+(set-face-attribute 'company-scrollbar-bg nil
+                    :background "gray40")
