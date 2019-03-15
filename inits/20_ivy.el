b3b3dc2aec1a479f63c05de236fb0c456a0c35dc
kkimigawa <kkimigawa@gmail.com>
2019-03-16 01:56:40 +0900

    [change] Change ivy key binding

--- inits/20_ivy.el
+++ inits/20_ivy.el
@@ -7,11 +7,13 @@
 ;; C-x bだけでなくC-x C-bでもバッファ変更できるようにする
 (global-set-key (kbd "C-x C-b") 'ivy-switch-buffer)
 
+(global-set-key (kbd "C-r") 'counsel-recentf)
+(global-set-key (kbd "C-s") 'swiper)
+
 (global-set-key (kbd "C-c i") 'counsel-imenu)
 (global-set-key (kbd "C-c f") 'counsel-git)
 (global-set-key (kbd "C-c g") 'counsel-git-grep)
 (global-set-key (kbd "C-c r") 'ivy-resume)
-(global-set-key (kbd "C-c s") 'swiper)
 
 ;; ripgrepが使用できる場合はgit grepではなくrgを使用する
 (when (executable-find "rg")
