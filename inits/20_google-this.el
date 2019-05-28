16f80f3c984aebd24c9e2c56bb8dfc7f8b8c7ce4
kkimigawa <kkimigawa@gmail.com>
2019-05-29 00:25:50 +0900

    [change] Change suffix in google-this-location-suffix
    * Remove duration in suffix
    * Remove language in suffix

--- inits/20_google-this.el
+++ inits/20_google-this.el
@@ -3,4 +3,4 @@
 (setq google-this-location-suffix "co.jp")
 (defun google-this-url () "URL for google searches."
   (concat google-this-base-url google-this-location-suffix
-          "/search?q=%s&hl=ja&num=10&as_qdr=y5&lr=lang_ja"))
+          "/search?q=%s&hl=ja&num=10"))
