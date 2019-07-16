45ad839c3da11c864fa22204e0f793d044d2e0e2
kkimigawa <kkimigawa@gmail.com>
2019-07-17 01:25:23 +0900

    [change] Change git-gutter color settings

--- inits/20_git-gutter.el
+++ inits/20_git-gutter.el
@@ -1,1 +1,6 @@
 (global-git-gutter-mode t)
+
+;; 色設定
+(set-face-background 'git-gutter:added  "green")
+(set-face-background 'git-gutter:deleted  "yellow")
+(set-face-background 'git-gutter:modified "magenta")
