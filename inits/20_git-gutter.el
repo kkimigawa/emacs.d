a24cf6ef1827e44a82cc3d4b80b5cb8668940912
kkimigawa <kkimigawa@gmail.com>
2020-07-30 01:41:36 +0900

    git-gutterのCLIで見たときのカラー調整

--- inits/20_git-gutter.el
+++ inits/20_git-gutter.el
@@ -1,10 +1,10 @@
 (global-git-gutter-mode t)
 
-;; 色設定
-(set-face-foreground 'git-gutter:added  "green")
-(set-face-foreground 'git-gutter:deleted  "red")
-(set-face-foreground 'git-gutter:modified "yellow")
+(setq git-gutter:added-sign " ")
+(setq git-gutter:deleted-sign " ")
+(setq git-gutter:modified-sign " ")
 
+;; 色設定
 (set-face-background 'git-gutter:added  "green")
 (set-face-background 'git-gutter:deleted  "red")
 (set-face-background 'git-gutter:modified "yellow")
