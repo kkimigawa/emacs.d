126a7354817026dd872e1243a2612f7c59f391ac
kkimigawa <kkimigawa@gmail.com>
2020-07-30 00:54:59 +0900

    melpa.milkbox.netに接続できなくなったのでMELPAのURL変更

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/")  t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/")  t)
 (package-initialize)
 
 (eval-when-compile (require 'cl))
