4d24da0e000bd9267b29fe4c135562bd939b29e8
kkimigawa <kkimigawa@gmail.com>
2020-07-30 01:08:41 +0900

    git-gutterのカラー変更

--- inits/20_git-gutter.el
+++ inits/20_git-gutter.el
@@ -1,6 +1,10 @@
 (global-git-gutter-mode t)
 
 ;; 色設定
+(set-face-foreground 'git-gutter:added  "green")
+(set-face-foreground 'git-gutter:deleted  "red")
+(set-face-foreground 'git-gutter:modified "yellow")
+
 (set-face-background 'git-gutter:added  "green")
-(set-face-background 'git-gutter:deleted  "yellow")
-(set-face-background 'git-gutter:modified "magenta")
+(set-face-background 'git-gutter:deleted  "red")
+(set-face-background 'git-gutter:modified "yellow")
