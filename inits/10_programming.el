0759ac0b504682d79de88368fb8d9e5ba5d5a2e8
kkimigawa <kkimigawa@gmail.com>
2020-12-28 14:40:08 +0900

    C言語の字下げ設定をK&Rに変更

--- inits/10_programming.el
+++ inits/10_programming.el
@@ -3,3 +3,9 @@
 
 ;; .mmをobjc-modeで開く
 (add-to-list 'auto-mode-alist '("\\.mm?$" . objc-mode))
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (c-set-style "k&r")
+            (setq c-basic-offset 4)
+            (c-set-offset 'statement-cont 'c-lineup-math)))
