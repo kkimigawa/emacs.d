e79bafb8cdfceeb36ad899da8cd36282e65e3f44
kkimigawa <kkimigawa@gmail.com>
2021-03-23 01:14:23 +0900

    magitを同じウインドウで開く方法を変更

--- inits/20_magit.el
+++ inits/20_magit.el
@@ -14,19 +14,11 @@
 (remove-hook 'server-switch-hook 'magit-commit-diff)
 
 ;; 同じウインドウでmagitを開く
-(setq magit-display-buffer-function
-      (lambda (buffer)
-        (display-buffer
-         buffer
-         (cond ((and (derived-mode-p 'magit-mode)
-                     (eq (with-current-buffer buffer major-mode)
-                         'magit-status-mode))
-                nil)
-               ((memq (with-current-buffer buffer major-mode)
-                      '(magit-process-mode
-                        magit-revision-mode
-                        magit-diff-mode
-                        magit-stash-mode))
-                nil)
-               (t
-                '(display-buffer-same-window))))))
+;; https://idiomdrottning.org/magit-transients
+(defun magit-display-buffer-same-window (buffer)
+  "Display BUFFER in the selected window like God intended."
+  (display-buffer
+   buffer '(display-buffer-same-window)))
+
+(setq magit-display-buffer-function 'magit-display-buffer-same-window)
+(setq magit-popup-display-buffer-action '((display-buffer-same-window)))
