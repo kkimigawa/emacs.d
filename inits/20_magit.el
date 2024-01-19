111cfb9bc8a232b92d54244b110016742a0d867c
kkimigawa <kkimigawa@gmail.com>
2024-01-19 22:53:54 +0900

    Display git tag list in magit.

--- inits/20_magit.el
+++ inits/20_magit.el
@@ -16,9 +16,6 @@
 ;; コミット時に変更点を表示しない(変更点が多いと重くなる)
 (remove-hook 'server-switch-hook 'magit-commit-diff)
 
-;; tagを表示しないことで高速化
-(remove-hook 'magit-refs-sections-hook 'magit-insert-tags)
-
 ;; 同じウインドウでmagitを開く
 ;; https://idiomdrottning.org/magit-transients
 (defun magit-display-buffer-same-window (buffer)
